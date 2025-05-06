c757c03784012b12cca04851bd6c22946119d8b4
yeinAlexa3012 <yein301206@gmail.com>
2025-05-06 16:09:19 -0500

    TC02 completo

--- cypress/e2e/TC02SolicitudServicio.cy.js
+++ cypress/e2e/TC02SolicitudServicio.cy.js
@@ -1,18 +1,49 @@
 describe('TC02: Solicitud de Servicio', () => {
-    it('Debería enviar la solicitud con los datos requeridos y recibir mensaje de confirmación', () => {
+    it('Debería enviar la solicitud con los datos requeridos y recibir mensaje de confirmacion', () => {
         // Visitar la URL principal
-        cy.visit('https://elitecatering.netlify.app/');
+        cy.visit('https://elitecatering.netlify.app/')
+        //Validar el tipo de letra
+        cy.get('h1').should('have.css', 'font-family', '"Times New Roman", Times, serif');
+        // Validar el titulo de la pagina 
+        cy.title().should('eq', 'ECS')
+        // Hacer click en el boton de servicios
+        cy.get(':nth-child(2) > a > strong').click()
+        // Hacer click en el boton de "mas" de servicios
+        cy.get(':nth-child(1) > .img_content > .img_servicio1 > .btn_info > a').click()
+        // Hacer click en el boton de "solicitar servicio"
+        cy.get('.boton_servicio > a').click()
+        // Llenar el nombre
+        cy.get('#nombreCliente').type('Juan Perez')
+        //Llenar el telefono
+        cy.get('#telefono').type('123456789')
+        //Llenar email 
+        cy.get('#email').type('juan@gmail.com')
+        //Llenar fecha
+        cy.get('#fechaEvento').type('2023-10-11')
+        //LLenar Tipo de Evento
+        cy.get('#tipoEvento').select('Boda')
+        //Llenar Hora 
+        cy.get('#horaEvento').type('15:00')
+        //Llenar Cantidad de Personas
+        cy.get('#invitados').type('100')
+        //Seleccionar servicio adicional
+        cy.get('#decoracion').click()
+        //Llenar mensaje adicional
+        cy.get('#mensaje').type('Necesito un servicio de catering para una boda')
+        //Hacer click en el boton de enviar
+        cy.get('.btn').click()
+        //Validar que el mensaje de confirmacion sea correcto
+        cy.get('#mensajeBienvenida')
+        //Validar contenido del mensaje de solicitud
+        cy.get('#mensajeBienvenida')
+        //Validar que el mensaje de confirmacion sea visible y contenga el texto esperado
+        .should('be.visible')
+        .should('contain', 'Solicitud Enviada')
+        .should('contain', '¡Su solicitud sera revisada en breve!');
+        
+        
 
-        // Hacer clic en el botón de servicios
-        cy.get(':nth-child(2) > a > strong').click();
+        
 
-        // Hacer clic en el botón de "más" del primer servicio
-        cy.get(':nth-child(1) > .img_content > .img_servicio1 > .btn_info > a').click();
-
-        // Hacer clic en el botón de "solicitar servicio"
-        cy.get('.boton_servicio > a').click();
-
-        // Llenar el formulario de solicitud
-        cy.get('#nombreCliente').type('Alexa');
-    });
-});
+    })
+})
