3f80a0d6bd2de3b390c8e3206eda1424b482d5b1
Dayana Valbuena <dvalbuenax@gmail.com>
2025-05-05 20:05:23 -0500

    first commit - pruebas Cypress para Elite Catering

---
+++ pruebas-elite/cypress/e2e/TC01InicioSesion.cy.js
@@ -0,0 +1,6 @@
+describe('TC01: Inicio de Sesión', () => {
+    it('Debería iniciar sesión correctamente y validar elementos', () => {
+        // Visitar la página de inicio de sesión
+        cy.visit('');
+    })
+})
