f8db71131a7bf699af314fede3699d830989eae7
Dayana Valbuena <dvalbuenax@gmail.com>
2025-05-06 18:50:46 -0500

    TC01 y TC02 funcionan correctamente, se deja archivo nombrado para TC03

--- cypress/e2e/TC01InicioSesion.cy.js
+++ cypress/e2e/TC01InicioSesion.cy.js
@@ -1,36 +1,41 @@
 describe('TC01: Inicio de Sesión', () => {
     it('Debería validar elementos y dar un mensaje de ingreso exitoso', () => {
-        // Visitar la URL principal
-        cy.visit('https://elitecatering.netlify.app/')
+        // PASO 1: Visitar la URL principal
+        cy.visit('https://elitecatering.netlify.app/')//cy.visit('http://127.0.0.1:5500/index.html')
         
-        // Validar que la URL sea correcta
-        cy.url().should('eq', 'https://elitecatering.netlify.app/')
+        // PASO 2: Validar que la URL sea correcta
+        cy.url().should('eq', 'https://elitecatering.netlify.app/')//cy.url().should('eq', 'http://127.0.0.1:5500/index.html')
 
-        //Validar que el título de la página sea correcto
-        cy.get('h1').should('have.text', 'Elite Catering Service')
+        // PASO 3: Validar que el título de la página sea correcto
+        cy.get('h1').should('have.text', 'ELITE CATERING SERVICE')
 
-        // Hacer clic en el botón de inicio de sesión
-        cy.contains('Iniciar Sesión').click()
+        // PASO 4: Hacer clic en el botón de inicio de sesión
+        cy.get('.btnIniciar > a').click()
 
-        // Validar que la URL de inicio de sesión sea correcta y tenga el path de login
-        cy.url().should('include', '/iniciarsesion')
+        // PASO 5: Validar que la URL de inicio de sesión sea correcta y tenga el path de login
+        cy.url().should('include', '/iniciarsesion')//cy.url().should('include', '/iniciarSesion.html')
 
-        // Validar que los campos esten presentes y visibles
+        // PASO 6: Validar que los campos esten presentes y visibles
         cy.get('#email').should('be.visible')
         cy.get('#password').should('be.visible')
 
-        // Validar que los valores igresados sean correctos
+        // PASO 7: Validar que los valores igresados sean correctos
         cy.get('#email').type('elitecateringservice@gmail.com')
         cy.get('#password').type('12345678')
 
-        // Validar estilos del boton de inicio de sesión
+        // PASO 8: Validar mensaje de recuperación de contraseña
+        cy.get('#enlaceRecuperacion').should('be.visible')
+        cy.get('#mensajeRecuperacion').should('have.text', 
+            'Comprueba tu dispositivo móvil.Hemos enviado un enlace para restablecer tu contraseña.')
+
+        // PASO 9: Validar estilos del boton de inicio de sesión
         cy.get('button[type="submit"]').should('have.css', 'background-color', 'rgb(38, 41, 39)')
 
-        // Hacer clic en el botón de inicio de sesión
+        // PASO 10: Hacer clic en el botón de inicio de sesión
         cy.get('button[type="submit"]').click()
 
-        //Validar mensaje de bienvenida
+        // PASO 11: Validar mensaje de bienvenida
         cy.get('#mensajeBienvenida').should('be.visible')
-        cy.get('#mensajeBienvenida').should('have.text', 'Inicio de Sesion Exitoso: ¡Bienvenido a Elite Catering Service!')
+        cy.get('#mensajeBienvenida').should('have.text', 'Inicio de Sesión Exitoso')
     })
 })
--- cypress/e2e/TC02SolicitudServicio.cy.js
+++ cypress/e2e/TC02SolicitudServicio.cy.js
@@ -1,49 +1,62 @@
 describe('TC02: Solicitud de Servicio', () => {
     it('Debería enviar la solicitud con los datos requeridos y recibir mensaje de confirmacion', () => {
-        // Visitar la URL principal
+        // PASO 1:Visitar la URL principal
         cy.visit('https://elitecatering.netlify.app/')
-        //Validar el tipo de letra
-        cy.get('h1').should('have.css', 'font-family', '"Times New Roman", Times, serif');
-        // Validar el titulo de la pagina 
+
+        // PASO 2:  Validar el tipo de letra
+        cy.get('h1').should('have.css', 'font-family', '"Times New Roman", Times, serif')
+
+        // PASO 3:  Validar el titulo de la pagina 
         cy.title().should('eq', 'ECS')
-        // Hacer click en el boton de servicios
+
+        // PASO 4:  Hacer click en el boton de servicios
         cy.get(':nth-child(2) > a > strong').click()
-        // Hacer click en el boton de "mas" de servicios
+
+        // PASO 5:  Hacer click en el boton de "mas" de servicios
         cy.get(':nth-child(1) > .img_content > .img_servicio1 > .btn_info > a').click()
-        // Hacer click en el boton de "solicitar servicio"
+
+        // PASO 6:  Hacer click en el boton de "solicitar servicio"
         cy.get('.boton_servicio > a').click()
-        // Llenar el nombre
+
+        // PASO 7:  Llenar el nombre
         cy.get('#nombreCliente').type('Juan Perez')
-        //Llenar el telefono
+
+        // PASO 8: Llenar el telefono
         cy.get('#telefono').type('1234567899')
-        //Llenar email 
+
+        // PASO 9: Llenar email 
         cy.get('#email').type('juan@gmail.com')
-        //Llenar fecha
+
+        // PASO 10: Llenar fecha
         cy.get('#fechaEvento').type('2023-10-12')
-        //LLenar Tipo de Evento
+
+        // PASO 11: LLenar Tipo de Evento
         cy.get('#tipoEvento').select('Boda')
-        //Llenar Hora 
+
+        // PASO 12: Llenar Hora 
         cy.get('#horaEvento').type('15:00')
-        //Llenar Cantidad de Personas
+
+        // PASO 13: Llenar Cantidad de Personas
         cy.get('#invitados').type('100')
-        //Seleccionar servicio adicional
+
+        // PASO 14: Seleccionar servicio adicional
         cy.get('#decoracion').click()
-        //Llenar mensaje adicional
+
+        // PASO 15: Llenar mensaje adicional
         cy.get('#mensaje').type('Necesito un servicio de catering para una boda')
-        //Hacer click en el boton de enviar
+
+        // PASO 16: Hacer click en el boton de enviar
         cy.get('.btn').click()
-        //Validar que el mensaje de confirmacion sea correcto
+        
+        // PASO 17: Validar que el mensaje de confirmacion sea correcto
         cy.get('#mensajeBienvenida')
-        //Validar contenido del mensaje de solicitud
+
+        // PASO 18: Validar contenido del mensaje de solicitud
         cy.get('#mensajeBienvenida')
-        //Validar que el mensaje de confirmacion sea visible y contenga el texto esperado
+
+        // PASO 19: Validar que el mensaje de confirmacion sea visible y contenga el texto esperado
         .should('be.visible')
         .should('contain', 'Solicitud Enviada')
         .should('contain', '¡Su solicitud sera revisada en breve!');
-        
-        
-
-        
-
     })
 })
